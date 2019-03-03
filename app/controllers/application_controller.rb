f02d547369805776e7b7525cc9aba89e3f9da2a6
Marie Burns <mcburns2222@gmail.com>
2019-03-03 06:35:17 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,22 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :super_hero
+    end
+    
+    post '/teams' do
+      params
+      binding.pry
+      # @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
+      
+      # params[:team][:members].each do |hero|
+      #   Hero.new(hero)
+      # end
+      
+      # @heros = Hero.all
+      
+      erb :team
+    end
 
 end
